8ce352a0d17b5f47c9896f4f791efff89d3de0b8
hp <hp@DESKTOP-6E2N1Q9>
2025-03-23 18:27:24 -0700

    SignUp

---
+++ Company.PL/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Company.PL.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region SignUp
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(SignUpDTO upDTO)
+        {
+            if(ModelState.IsValid)
+            {
+                // Add User To Database
+                return RedirectToAction("SignIn");
+            }
+
+            return View();
+        }
+        #endregion
+
+        #region SingIn
+
+        #endregion
+
+
+        #region SingOut
+
+        #endregion
+    }
+}
---
+++ Company.PL/DTOs/SignUpDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.PL.DTOs
+{
+    public class SignUpDTO
+    {
+        [Required(ErrorMessage = "FirstName Is Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName Is Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "UserName Is Required")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Email Is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "ConfirmPassword Is Required")]
+        [DataType(DataType.Password)]
+
+        [Compare("Password", ErrorMessage = "Password And ConfirmPassword Do Not Match")]
+        public string ConfirmPassword { get; set; }
+        public bool? IsAgree { get; set; }
+    }
+}
